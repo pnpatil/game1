0a81bf2d80221ad688be929a8449cf8e6532b0cd
pnpatil <91815619+pnpatil@users.noreply.github.com>
2021-10-03 09:35:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+var numberOFDrums = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < numberOFDrums;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+
+    var innerHTML = this.innerHTML;
+    makeSound(innerHTML);
+    animationOFCLick(innerHTML);
+  });
+}
+
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+  animationOFCLick(event.key);
+});
+
+function animationOFCLick(key){
+  var par = "." + key;
+  document.querySelector(par).classList.add("pressed");
+
+  setTimeout(function () {
+    document.querySelector(par).classList.remove("pressed");
+  }, 100);
+}
+
+function makeSound(key) {
+  switch(key) {
+    case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+      break;
+    case "a":
+        var kick_bass = new Audio("sounds/kick-bass.mp3");
+        kick_bass.play();
+      break;
+    case "s":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+      break;
+    case "d":
+        var tom_1 = new Audio("sounds/tom-1.mp3");
+        tom_1.play();
+      break;
+    case "j":
+        var tom_2 = new Audio("sounds/tom-2.mp3");
+        tom_2.play();
+      break;
+    case "k":
+        var tom_3 = new Audio("sounds/tom-3.mp3");
+        tom_3.play();
+      break;
+    case "l":
+        var tom_4 = new Audio("sounds/tom-4.mp3");
+        tom_4.play();
+      break;
+  }
+}
